cca035767dae06020b592f8284feee3327d71e85
emonShimoul <emonshimoul@gmail.com>
2022-06-22 12:40:38 +0600

    added responsive design

--- script.js
+++ script.js
@@ -5,5 +5,22 @@ let slider = tns({
     "nav": false,
     autoplay: true,
     controls: true,
-    autoplayButtonOutput: false
+    autoplayButtonOutput: false,
+    responsive: {
+        1600: {
+            items: 4,
+            gutter: 20
+        },
+        1024: {
+            items: 3,
+            gutter: 20
+        },
+        768: {
+            items: 2,
+            gutter: 20
+        },
+        480: {
+            items: 1
+        }
+    }
 })
